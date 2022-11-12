fcec5457d555d9c275ebd81f4d8bc888de46e9ea
Vishal <vishallokare11502@gmail.com>
2022-11-12 17:15:42 +0530

    Removed unnecessary functions

--- driver/lib/pages/place_service.dart
+++ driver/lib/pages/place_service.dart
@@ -66,23 +66,6 @@ class PlaceApiProvider {
   final Set<Polyline> _polyLines = {};
   Set<Polyline> get polyLines => _polyLines;
 
-  void createRoute(String encondedPoly) { //, String latLng) {
-    _polyLines.add(Polyline(
-        polylineId: PolylineId('1'),
-        width: 4,
-        points: _convertToLatLng(_decodePoly(encondedPoly)),
-        color: Colors.red));
-    // _addMarker(destination, "KTHM Collage");
-  }
-
-  Future<Response> getRouteCoordinates(LatLng l1, LatLng l2) async {
-    String url =
-        "https://maps.googleapis.com/maps/api/directions/json?origin=${l1.latitude},${l1.longitude}&destination=${l2.latitude},${l2.longitude}&key=$apiKey";
-    Response response = await client.get(Uri.parse(url));
-    return response;
-    Map values = jsonDecode(response.body);
-    return values["routes"][0]["overview_polyline"]["points"];
-  }
 
   Future<List<Suggestion>> fetchSuggestions(String input) async {
     String request =
@@ -106,21 +89,6 @@ class PlaceApiProvider {
     }
   }
 
-  Future<dynamic> getLatLng(String resultsPlaceId) async {
-    String query = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=$resultsPlaceId&key=$apiKey&session=$sessionToken';
-    Response response = await client.get(Uri.parse(query));
-    if(response.statusCode == 200){
-      final result = json.decode(response.body);
-      if(result['status'] == 'OK'){
-        return LatLng(result['result']['geometry']['location']['lat'], result['result']['geometry']['location']['lng']);
-      }
-    }
-    // throw Exception(result['error_message']);
-    return null;
-    // Map result = jsonDecode(response.body);
-    // LatLng latLng = LatLng(result['result']['geometry']['location']['lat'], result['geometry']['location']['lat']);
-    // return latLng;
-  }
 
   // Future<Place> getPlaceDetailFromId(String placeId) async {
   //   // if you want to get the details of the selected place by place_id
