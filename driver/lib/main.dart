647fa4b9b344eba7f5f27ef2513a0cfc99eb2bc5
Vishal <vishallokare11502@gmail.com>
2022-11-13 17:31:10 +0530

    Added a login page

--- driver/lib/main.dart
+++ driver/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:driver/pages/login.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:driver/pages/map.dart';
@@ -18,9 +20,10 @@ class Driver extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.system,
-      initialRoute: '/map',
+      initialRoute: '/login',
       routes: <String, WidgetBuilder>{
         "/map" : (BuildContext ctx) => Map(),
+        "/login" : (BuildContext ctx) => Login(),
       },
     );
   }
